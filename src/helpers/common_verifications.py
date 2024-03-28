bc77e7b22ad77214a7cba31452aa3ba04ebf4d91
swapnali.mali <smali6355@gmail.com>
2024-03-28 10:57:16 +0530

    class 11th Dec23 CRUD API Framework code added

--- src/helpers/common_verifications.py
+++ src/helpers/common_verifications.py
@@ -8,7 +8,7 @@ def verify_json_key_for_not_null(key):
     assert key >= 0, "key is not zero"
 
 
-def verify_response(key):
+def verify_response_key_should_not_be_none(key):
     assert key is not None, "token is not none"  #check for token should be not null
 
 
--- tests/Integration_tests/test_create_booking.py
+++ tests/Integration_tests/test_create_booking.py
@@ -6,21 +6,22 @@ from src.helpers.api_requests_wrapper import post_request
 from src.constants.api_constants import create_booking
 from src.helpers.payload_manager import payload_create_booking
 from src.helpers.utils import common_headers_json
-from src.helpers.common_verifications import verify_response,verify_http_status_code
+from src.helpers.common_verifications import verify_response_key_should_not_be_none,verify_http_status_code
 
 
 class TestCreateBooking(object):
 
     def test_create_booking_tc1(self):
-        response = post_request(url= create_booking(),auth = None,headers= common_headers_json(),payload = payload_create_booking(),in_json= False) #want response in json
-        print(create_booking())
-        print(common_headers_json())
-        print(payload_create_booking())
-        print(response)
+        response = post_request(url= create_booking(),auth = None,headers= common_headers_json(),payload = payload_create_booking(), in_json =False) #want response in json
+        # print(create_booking())
+        # print(common_headers_json())
+        # print(payload_create_booking())
+        #print(response)
 
-        verify_response(response["token"]) # check token is not null
+        verify_response_key_should_not_be_none(response.json()["bookingid"]) # check for getting the booking id
         #verify_http_status_code(response,200) #to check if the status code is correct
-
+        bookingid = response.json()["bookingid"]
+        print(bookingid)
 
 
 
